21588570da4f3db0c3a79fed5da94d9270c42383
Sze Chuan Lai <sze.chuan@gmail.com>
2017-12-13 13:59:01 +0800

    Section 2 Exercise Updates

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -35,15 +35,32 @@ namespace Vidly.Controllers
             return Content(year + "/" + month);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+            var movies = GetMovies();
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+            return View(movies);
+        }
 
-            return Content(String.Format("pageIndex={0}&sortByName={1}", pageIndex, sortBy));
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().FirstOrDefault(i => i.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            //return new List<Movie>
+            //{
+            //    new Movie(){ Id = 1, Name= "Shrek"},
+            //    new Movie(){ Id = 2, Name = "Wall-e"}
+            //};
+
+            return new List<Movie>();
         }
     }
 }

b842f86dd56257cfe870ad04d21cb50b51fd488a
Sze Chuan <sze.chuan@gmail.com>
2017-12-19 14:39:07 +0800

    Final Touch Up

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -18,6 +18,8 @@ namespace Vidly.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
+            throw new Exception();
+
             return View();
         }
 
